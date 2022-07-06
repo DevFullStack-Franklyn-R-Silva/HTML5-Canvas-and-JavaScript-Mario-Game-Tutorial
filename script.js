49b2ab1b18dcfa7d8b78bc99475bf6080e8efe42
Franklyn R <franklyn1998@live.com>
2022-07-06 15:27:12 -0300

    Player movement

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ class Player {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height) {
@@ -39,12 +40,72 @@ class Player {
     }
 }
 const player = new Player();
-
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    },
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5;
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5;
+    }
+    else player.velocity.x = 0;
 }
 
 animate()
+
+window.addEventListener("keydown", ({ keyCode }) => {
+    // console.log(keyCode);
+    switch (keyCode) {
+        case 65:
+            console.log("left")
+            keys.left.pressed = true
+            break;
+        case 83:
+            console.log("down")
+            break;
+        case 68:
+            console.log("right")
+            keys.right.pressed = true
+            break;
+        case 87:
+            console.log("up")
+            player.velocity.y -= 20
+            break;
+    }
+    // console.log(keys.right.pressed)
+    // console.log(keys.left.pressed)
+})
+
+window.addEventListener("keyup", ({ keyCode }) => {
+    // console.log(keyCode);
+    switch (keyCode) {
+        case 65:
+            console.log("left")
+            keys.left.pressed = false
+            break;
+        case 83:
+            console.log("down")
+            break;
+        case 68:
+            console.log("right")
+            keys.right.pressed = false
+            break;
+        case 87:
+            console.log("up")
+            player.velocity.y -= 20
+            break;
+    }
+    // console.log(keys.right.pressed)
+    // console.log(keys.left.pressed)
+})
